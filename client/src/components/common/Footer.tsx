a45811771d08340cff25269476dedae9f28e6384
Rishabh Virmani <rishabhvirmani28@gmail.com>
2025-04-14 18:20:32 +0530

    Fresh start: My code only

---
+++ client/src/components/common/Footer.tsx
@@ -0,0 +1,17 @@
+function Footer() {
+    return (
+        <footer className="static bottom-1 left-0 flex w-full justify-center sm:fixed">
+            <span>
+                Build with ❤️ by{" "}
+                <a
+                    href="https://github.com/RishabhVirmani17"
+                    className="text-primary underline underline-offset-1"
+                >
+                    Rishabh Virmani
+                </a>
+            </span>
+        </footer>
+    )
+}
+
+export default Footer
